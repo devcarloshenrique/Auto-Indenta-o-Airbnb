9ed875ce22f55591333c327095e7f8abf6bd058b
devcarloshenrique <devcarloshenrique@gmail.com>
2020-02-03 15:27:30 -0300

    refactor: eslint configuration and pritters

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,8 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: [ 'airbnb-base', 'prettier' ],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -16,15 +15,9 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
-    // No eslint por padrão, todo metodo de uma class precisa usar this.  
     "class-methods-use-this": "off",
-    // No eslint por padrão, não é possível receber parametros e fazer alterações 
-    "no-param-reassign" : "off",
-    // Por padrão o eslint não permite que variáveis sejá criada desta manéira variavel_impossible
+    "no-param-reassing" : "off",
     "camelcase": "off",
-    /* Por padrão o eslint não permite declarar uma variável e não utiliza-la Ele vai ignorar a variavel
-    next, caso eu chame ela como parametro e não use-a. 
-    */
     "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
   },
 };
